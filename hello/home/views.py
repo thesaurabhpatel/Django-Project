02bb82f0f3959f6c963a361195a4268163da9556
thesaurabhpatel <patel99saurabh@gmail.com>
2020-05-09 13:30:18 +0530

    Styling of website

---
+++ hello/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def services(request):
+    return HttpResponse("this is service page")
+
+def contact(request):
+    return HttpResponse("this is contact page")
